a46c06a833b4dc992fd997e6e0893d7d5812b936
stkdiretto <stefan@shutterworks.org>
2016-11-14 19:23:04 +0100

    added niveau 2

--- js/uulm-map.js
+++ js/uulm-map.js
@@ -20,10 +20,9 @@ var map = new L.Map('map').addLayer(transitmap).setView(new L.LatLng(48.4218, 9.
 // *************************
 //OverPassAPI overlays
 
-// Zuerst alle Fahrradabstellplaetze, ab einem Zoomlevel von 15
-var bikeparking = new L.OverPassLayer({
+var niveau2 = new L.OverPassLayer({
       minzoom: 15,
-      query: "node(BBOX)[amenity=bicycle_parking];out;",
+      query: "way[indoor=room][level=2];out;",
       callback: function(data) {
         for(i=0;i<data.elements.length;i++) {
           e = data.elements[i];
@@ -39,98 +38,11 @@ var bikeparking = new L.OverPassLayer({
           if (e.tags.opening_hours) {popup = popup + 'Öffnungszeiten: ' + e.tags.opening_hours + '<br>'};
           if (e.tags.operator) {popup = popup + 'Betreiber: ' + e.tags.operator};
           popup = popup + '</div><a href="http://www.openstreetmap.org/edit?editor=id&node='+e.id+'">Eintrag mit iD-Editor bearbeiten</a><br>';
-
-          var myicon = L.icon({
-              iconUrl: 'img/transport_parking_bicycle.n.0092DA.20.png',
-              iconSize: [20, 20],
-              iconAnchor: [0, 10],
-              popupAnchor: [10, -12]
-          });
-          var marker = L.marker(pos, {icon: myicon}).bindPopup(popup);
           this.instance.addLayer(marker);
         }
       },
     });
 
-
-// Alle Warenautomaten
-
-var vending = new L.OverPassLayer({
-      minzoom: 15,
-      query: "node(BBOX)[amenity=vending_machine];out;",
-      callback: function(data) {
-        for(i=0;i<data.elements.length;i++) {
-          e = data.elements[i];
-          if (e.id in this.instance._ids) return;
-          this.instance._ids[e.id] = true;
-
-          var icon = 'img/shopping_vending_machine.glow.8E7409.32.png'
-          var pos = new L.LatLng(e.lat, e.lon);
-          var popup = '<b>Warenautomat</b><br><div>';
-          if (e.tags.vending == 'food') 
-                    { popup = popup + 'Speisen<br>';
-                      icon = 'img/food_pizza.glow.8E7409.32.png'; }
-          else if (e.tags.vending == 'sweets') 
-                    { popup = popup + 'Süßwaren<br>';
-                      icon = 'img/shopping_confectionery.glow.8E7409.32.png'; }
-          else if (e.tags.vending == 'drinks') 
-                    { popup = popup + 'Getränke<br>'; }
-          else if (e.tags.vending == 'ice_cream')
-                    { popup = popup + 'Eis<br>'; 
-                      icon = 'img/food_ice_cream.glow.8E7409.32.png'; }
-          else if (e.tags.vending == 'condoms')
-                    { popup = popup + 'Kondome<br>'; }
-          else { popup = popup + e.tags.vending + '<br>'; }
-                                          
-//          if (e.tags.payment:coins == 'yes') {popup = popup + 'Nimmt Münzen<br>'}; // geht net, wegen Doppelpunkt oO
-          if (e.tags.operator) {popup = popup + 'Betreiber: ' + e.tags.operator};
-          popup = popup + '</div><a href="http://www.openstreetmap.org/edit?editor=id&node='+e.id+'">Eintrag mit iD-Editor bearbeiten</a><br>';
-
-          var myicon = L.icon({
-              iconUrl: icon,
-              iconSize: [20, 20],
-              iconAnchor: [0, 10],
-              popupAnchor: [10, -12]
-          });
-          var marker = L.marker(pos, {icon: myicon}).bindPopup(popup);
-          this.instance.addLayer(marker);
-        }
-      },
-    });
-
-
-// Kunstpfad
-var artwork = new L.OverPassLayer({
-      minzoom: 15,
-//      [out:json][timeout:25];(node["tourism"="artwork"];way["tourism"="artwork"];relation["tourism"="artwork"];);out body;>;out skel qt;
-      query: "(node(BBOX)[tourism=artwork];way(BBOX)[tourism=artwork];relation(BBOX)[tourism=artwork];);out;",
-      callback: function(data) {
-        for(i=0;i<data.elements.length;i++) {
-          e = data.elements[i];
-          if (e.id in this.instance._ids) return;
-          this.instance._ids[e.id] = true;
-
-          var pos = new L.LatLng(e.lat, e.lon);
-          var popup = this.instance._poiInfo(e.tags,e.id);
-//          var popup = '<b>Fahrradabstellanlage</b><br><div>';
-//          if (e.tags.name) { popup = popup + e.tags.name + '<br>'};
-//          if ((e.tags.covered) && (e.tags.covered == 'yes')) {popup = popup + 'überdachte Anlage<br>'};
-//          if (e.tags.capacity) {popup = popup + e.tags.capacity + ' Plätze<br>'};
-//          if (e.tags.opening_hours) {popup = popup + 'Öffnungszeiten: ' + e.tags.opening_hours + '<br>'};
-//          if (e.tags.operator) {popup = popup + 'Betreiber: ' + e.tags.operator};
-//          popup = popup + '</div><a href="http://www.openstreetmap.org/edit?editor=id&node='+e.id+'">Eintrag mit iD-Editor bearbeiten</a><br>';
-
-          var myicon = L.icon({
-              iconUrl: 'img/tourist_attraction.n.DA0092.32.png',
-              iconSize: [20, 20],
-              iconAnchor: [0, 10],
-              popupAnchor: [10, -12]
-          });
-          var marker = L.marker(pos, {icon: myicon}).bindPopup(popup);
-          this.instance.addLayer(marker);
-        }
-      },
-    });
 
 
 var baseMaps = {
@@ -140,9 +52,7 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-   "Fahrradabstellplätze": bikeparking,
-   "Warenautomaten": vending,
-   "Kunstwerke": artwork
+   "Niveau 2": niveau2
 };
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
